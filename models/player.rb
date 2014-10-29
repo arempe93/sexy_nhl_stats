a1bc72ef40dd6171457c073b9fd9e51b43413f47
Andrew Rempe <andrewrempe@gmail.com>
2014-10-28 21:51:19 -0400

    Adds Player model

--- models/player.rb
+++ models/player.rb
@@ -1,15 +1,14 @@
-class Team < ActiveRecord::Base
+class Player < ActiveRecord::Base
 
 	# Callbacks
 
 	# Valiadations
 	validates :nhl_id, presence: true, uniqueness: true
-	validates :city, presence: true
+	validates :team_id, presence: true
 	validates :name, presence: true
-	validates :abbv, presence: true, uniqueness: true
 
 	# Relationships
-	has_many :players
+	belongs_to :team
 
 	# Functions
 end
