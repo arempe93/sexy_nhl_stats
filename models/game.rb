3a656e4228817bb60ccb55ace9ab4a1a54012ba1
Andrew Rempe <andrewrempe@gmail.com>
2014-10-28 22:10:43 -0400

    Adds Game model

---
+++ models/game.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id              :integer          not null, primary key
+#  nhl_id          :integer
+#  game_time       :datetime
+#  home_team_id    :integer
+#  home_team_score :integer
+#  away_team_id    :integer
+#  away_team_score :integer
+#  decision        :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
+class Game < ActiveRecord::Base
+
+	# Callbacks
+
+	# Validations
+
+	# Relationships
+	belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
+	belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
+
+	# Functions
+	def winner
+		home_team_score > away_team_score ? home_team : away_team
+	end
+end
