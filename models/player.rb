899b6948950edf2dc108de0f3836e401e3bb045b
Andrew Rempe <andrewrempe@gmail.com>
2014-10-28 21:51:57 -0400

    Annotates Player and Team models

--- models/player.rb
+++ models/player.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: players
+#
+#  id          :integer          not null, primary key
+#  nhl_id      :integer
+#  team_id     :integer
+#  name        :string(255)
+#  sweater     :integer
+#  player_type :string(255)
+#
+
 class Player < ActiveRecord::Base
 
 	# Callbacks
--- models/team.rb
+++ models/team.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :integer          not null, primary key
+#  nhl_id     :integer
+#  city       :string(255)
+#  name       :string(255)
+#  abbv       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Team < ActiveRecord::Base
 
 	# Callbacks
