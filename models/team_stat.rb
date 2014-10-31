9d5bbd11b7353f00f40c1e9a7afd92a769e21558
Andrew Rempe <andrewrempe@gmail.com>
2014-10-30 22:51:12 -0400

    Adds TeamStat model

---
+++ models/team_stat.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: team_stats
+#
+#  id        :integer          not null, primary key
+#  team_id   :integer
+#  game_id   :integer
+#  shots     :integer
+#  blocks    :integer
+#  pim       :integer
+#  hits      :integer
+#  fow       :integer
+#  takeaways :integer
+#  giveaways :integer
+#  penalties :string(255)
+#
+
+class TeamStat < ActiveRecord::Base
+
+	# Callbacks
+
+	# Validations
+
+	# Relationships
+	belongs_to :team
+	belongs_to :game
+
+	# Functions
+	def penalty_percentage
+		numbers = penalties.split '/'
+
+		numbers.first.to_i / numbers.last.to_i
+	end
+end
