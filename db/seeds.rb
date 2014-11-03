1edc3242c8fdff525c027fdd7e7443caa8b203d3
Andrew Rempe <andrewrempe@gmail.com>
2014-11-03 15:02:14 -0500

    Adds a seed file to use pullers, Adds a .gitignore

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# Redirect output to log file
+$stdout.reopen(File.expand_path('../../logs/seed.txt', __FILE__), 'w')
+
+# Run all pullers
+require_relative '../lib/pull_all'
+
+# Manually fix some team names
+mapleleafs = Team.find_by nhl_id: 10
+mapleleafs.name = 'Maple Leafs'		# was Leafs
+mapleleafs.city = 'Toronto' 		# was Toronto Maple
+mapleleafs.save
+
+redwings = Team.find_by nhl_id: 17
+redwings.name = 'Red Wings'			# was Wings
+redwings.city = 'Detroit'			# was Detroit Red
+redwings.save
+
+bluejackets = Team.find_by nhl: 29
+bluejackets.name = 'Blue Jackets'	# was Jackets
+bluejackets.city = 'Columbus'		# was Columbus Blue
+bluejackets.save
--- lib/pull_all.rb
+++ lib/pull_all.rb
@@ -1,7 +1,7 @@
 # Require all pullers
 require_relative 'pullers/pull_teams'
 require_relative 'pullers/pull_games'
-require_relative 'pullers/pull_players'
+require_relative 'pullers/pull_skaters'
 require_relative 'pullers/pull_goalies'
 require_relative 'pullers/pull_skater_stats'
 require_relative 'pullers/pull_goalie_stats'
