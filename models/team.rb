3edfb46df58cda32acb788e9f203db5133429722
Andrew Rempe <andrewrempe@gmail.com>
2014-11-03 20:03:23 -0500

    Adds record stats methods to Team model

--- models/team.rb
+++ models/team.rb
@@ -35,6 +35,38 @@ class Team < ActiveRecord::Base
 		Game.all.where("game_time < '#{(DateTime.now.midnight + 3.hours).strftime('%F %T %z')}' and (home_team_id = #{id} or away_team_id = #{id})")
 	end
 
+	def games_played
+		all_played_games.count
+	end
+
+	def wins
+		wins = 0
+		all_played_games.each do |game|
+			wins += 1 if game.winner.id == id
+		end
+		wins
+	end
+
+	def losses
+		losses = 0
+		all_played_games.each do |game|
+			losses += 1 if game.winner.id != id and game.decision == 'F'
+		end
+		losses
+	end
+
+	def ot
+		ot = 0
+		all_played_games.each do |game|
+			ot += 1 if game.winner.id != id and (game.decision == 'OT' or game.decision == 'SO') 
+		end
+		ot
+	end
+
+	def points
+		wins * 2 + ot
+	end
+
 	def logo_name
 		logo = name.gsub /\s/, ''
 
