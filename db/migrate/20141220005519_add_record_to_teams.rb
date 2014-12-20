c7dd18b464aba9d0d105efdb403b51ff4eecb8b9
celic <cceli@codequarry.net>
2014-12-19 19:58:00 -0500

    Adds record to teams in db

---
+++ db/migrate/20141220005519_add_record_to_teams.rb
@@ -0,0 +1,8 @@
+class AddRecordToTeams < ActiveRecord::Migration
+  def change
+  	add_column :teams, :wins, :integer, :default => 0
+  	add_column :teams, :losses, :integer, :default => 0
+  	add_column :teams, :ot, :integer, :default => 0
+  	add_column :teams, :row, :integer, :default => 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214213538) do
+ActiveRecord::Schema.define(version: 20141220005519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,10 @@ ActiveRecord::Schema.define(version: 20141214213538) do
     t.datetime "updated_at"
     t.string   "conference"
     t.string   "division"
+    t.integer  "wins",       default: 0
+    t.integer  "losses",     default: 0
+    t.integer  "ot",         default: 0
+    t.integer  "row",        default: 0
   end
 
 end
