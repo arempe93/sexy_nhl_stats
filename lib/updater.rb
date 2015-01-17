4aa85bd25c66c489c831a88c25cdf97856aa707c
celic <cceli@codequarry.net>
2015-01-17 01:12:02 -0500

    Stripping makes Andrew get the whip

--- lib/updater.rb
+++ lib/updater.rb
@@ -41,7 +41,7 @@ Game.unstored_games.each do |game|
 
 	# Open stats file
 	gcbx_file = open("http://live.nhl.com/GameData/20142015/#{game.nhl_id}/gc/gcbx.jsonp")
-	gcbx = JSON.parse(gcbx_file.read[10..-2])
+	gcbx = JSON.parse(gcbx_file.read.strip[10..-2])
 
 	# Open scoreboard file
 	game_stats_file = open("http://live.nhl.com/GameData/20142015/#{game.nhl_id}/gc/gcsb.jsonp")
